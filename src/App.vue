b5f179e8c1ea6353b702d16fba770f455d840452
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-05-29 15:22:34 -0600

    App pantalla principal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="">
       Hello world!
     </h1>
-    <hr>
-    <MyCounter :hulk="5" text="Ernesto"/>
-    <MyCounterScript :value="5" />
+    
 </template>
 
 <script lang="ts" setup>
-
-import MyCounter from './components/MyCounter.vue'
-import MyCounterScript from './components/MyCounterScript.vue'
 
 </script>
